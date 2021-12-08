4ba967133441dcb8cd30b29c08bf3083b896a73b
Tobiasz Chodarewicz <tobiasz@bjshomedelivery.com>
2021-12-08 08:14:15 +0000

    Add GET, PATCH and DELETE methods for single post

--- toby_dev_blog/api/resources/post/post_api.py
+++ toby_dev_blog/api/resources/post/post_api.py
@@ -1,11 +1,12 @@
 """`Post` views - module contains methods to operate on the single and multiple
 `Post` objects"""
+from flask import Response
 from flask.views import MethodView
 from flask_smorest import Blueprint
 
 from toby_dev_blog.extensions import db
 from toby_dev_blog.models import Post
-from toby_dev_blog.schemas import PostSchema
+from toby_dev_blog.schemas import PostPatchSchema, PostSchema
 
 blp_post = Blueprint(
     "posts", "posts", url_prefix="/posts", description="Operations on posts"
@@ -27,6 +28,7 @@ class PostListAPI(MethodView):
 
     @blp_post.arguments(PostSchema(many=True))
     @blp_post.response(200, PostSchema(many=True))
+    @blp_post.alt_response(422, description="Invalid payload")
     def post(self, post_data: list[Post]):
         """Post/create list of posts"""
         db.session.add_all(post_data)
@@ -34,6 +36,7 @@ class PostListAPI(MethodView):
         return post_data
 
 
+@blp_post.route("/<string:slug>")
 class PostAPI(MethodView):
     """Post object endpoint
 
@@ -41,14 +44,30 @@ class PostAPI(MethodView):
     :type MethodView: :class:`MethodView`
     """
 
-    def get(self):
-        """Retrive single post by slug or id"""
-        pass
+    @blp_post.response(200, PostSchema)
+    @blp_post.alt_response(404, description="Post not found")
+    def get(self, slug):
+        """Retrive single post by slug"""
+        return Post.query.filter(Post.slug == slug).first_or_404()
 
-    def patch(self):
+    @blp_post.arguments(PostPatchSchema)
+    @blp_post.response(200, PostSchema)
+    @blp_post.alt_response(404, description="Post not found")
+    @blp_post.alt_response(422, description="Invalid payload")
+    def patch(self, post_data, slug):
         """Edit single post object"""
-        pass
+        post = Post.query.filter(Post.slug == slug).first_or_404()
+        post_schema = PostSchema()
+        post = post_schema.load(post_data, instance=post, partial=True)
+        db.session.add(post)
+        db.session.commit()
+        return post
 
-    def delete(self):
+    @blp_post.response(204)
+    @blp_post.alt_response(404, description="Post not found")
+    def delete(self, slug):
         """Delete a post"""
-        pass
+        post = Post.query.filter(Post.slug == slug).first_or_404()
+        db.session.delete(post)
+        db.session.commit()
+        return Response(status=204)
